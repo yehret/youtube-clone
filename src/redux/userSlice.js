523235e2b234662824d1da471d9e459163ecb061
Serhii <sirozhka.v@gmail.com>
2024-02-05 13:24:08 +0100

    redux store configuration

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-   user: null,
+   currentUser: null,
    loading: false,
    error: false
 }
@@ -15,14 +15,14 @@ export const userSlice = createSlice({
       },
       loginSuccess: (state, action) => {
          state.loading = false;
-         state.user = action.payload
+         state.currentUser = action.payload
       },
       loginFailure: (state) => {
          state.loading = false;
          state.error = true;
       },
       logout: (state) => {
-         state.user =  null;
+         state.currentUser =  null;
          state.loading = false;
          state.error= false;
       }
