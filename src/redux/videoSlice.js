5bad95b5afad9cf0e4e362af3523c246b3a9e8b7
Serhii <sirozhka.v@gmail.com>
2024-02-05 13:48:28 +0100

    redux persist configuration

---
+++ src/redux/videoSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+   video: null,
+   loading: false,
+   error: false
+}
+
+export const videoSlice = createSlice({
+   name: 'video',
+   initialState,
+   reducers: {
+      loginStart: (state) => {
+         state.loading = true;
+      },
+      loginSuccess: (state, action) => {
+         state.loading = false;
+         state.currentUser = action.payload
+      },
+      loginFailure: (state) => {
+         state.loading = false;
+         state.error = true;
+      },
+      logout: (state) => {
+         state.currentUser =  null;
+         state.loading = false;
+         state.error= false;
+      }
+   }
+})
+
+export const {loginStart, loginSuccess, loginFailure, logout} = videoSlice.actions
+
+export default videoSlice.reducer
